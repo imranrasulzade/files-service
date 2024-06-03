b50add4ef0d9d31b73494023ebad3746e68bde72
imranrasulzade <imranrasulzade5@gmail.com>
2024-06-03 22:33:27 +0400

    initial git

---
+++ src/test/java/com/rsl/filesservice/FilesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rsl.filesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
